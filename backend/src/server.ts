45cc085d4beb4d8c509b71617a835704a4cf5d8c
urba <urba@iMac-de-urba.local>
2025-12-04 13:44:32 +0100

    accor orgine url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -128,7 +128,14 @@ const startServer = async () => {
     const app = express();
     const httpServer = http.createServer(app);
 
-    app.use(cors({ origin: true, credentials: true }));
+    app.use(cors({
+    origin: [
+        "http://localhost:3000", 
+        "http://localhost:5002", 
+        "https://backoffice.urbagroupe.ma" // ✅ ZID HADI
+    ],
+    credentials: true
+}));ƒ
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
